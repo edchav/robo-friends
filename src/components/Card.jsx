54b36fcb57e142dbf2a29f69bc218de03621663e
edchav <edgar.chavez02@outlook.com>
2025-06-12 22:38:56 -0500

    fixed displaying card issue

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Card = ({ name, email, id}) => {
     return (
-        <div className='tc bg-light-green br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='tc dib bg-light-green br3 pa3 ma2 grow bw2 shadow-5'>
             <img src={`https://robohash.org/${id}?200x200`} alt='robots'></img>
             <div>
                 <h2>{name}</h2>
