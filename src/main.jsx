5d68604a513fc5e1b3b4ac9027834d4dc3a10491
edchav <edgar.chavez02@outlook.com>
2025-06-14 17:32:55 -0500

    connect react to redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import 'tachyons';
+import { Provider  } from 'react-redux';
+import { createStore } from 'redux';
+import { searchRobotsReducer } from './reducers';
 import { StrictMode } from 'react';
 import App from './containers/App';
 
+const store = createStore(searchRobotsReducer);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store ={store}>
+      <App />
+    </Provider>
+    
   </StrictMode>
 )
  
