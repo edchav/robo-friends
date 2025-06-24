8b279e0654014baa7a7151f26c3ac14dcf24f5cb
edchav <edgar.chavez02@outlook.com>
2025-06-24 16:58:48 -0500

    optimizations based on lighthouse

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Card = ({ name, email, id}) => {
     return (
         <div className='tc dib bg-light-green br3 pa3 ma2 grow bw2 shadow-5'>
-            <img src={`https://robohash.org/${id}?size=200x200`} alt='robots'/>
+            <img src={`https://robohash.org/${id}?size=200x200`} width={200} height={200} alt='robots'/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
